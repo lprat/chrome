380d1ed78527aa45e2a5cf6d52864ba9164c7d15
Lionel <lionel.prat9@gmail.com>
2021-07-13 16:33:00 +0200

    Update entrypoint.sh

--- copyables/entrypoint.sh
+++ copyables/entrypoint.sh
@@ -32,6 +32,6 @@ IFS='x' read SCREEN_WIDTH SCREEN_HEIGHT <<< "${VNC_SCREEN_SIZE}"
 export VNC_SCREEN="${SCREEN_WIDTH}x${SCREEN_HEIGHT}x24"
 export CHROME_WINDOW_SIZE="${SCREEN_WIDTH},${SCREEN_HEIGHT}"
 
-export CHROME_OPTS="${CHROME_OPTS_OVERRIDE:- --user-data-dir --no-sandbox --window-position=0,0 --force-device-scale-factor=1 --disable-dev-shm-usage}"
+export CHROME_OPTS="${CHROME_OPTS_OVERRIDE:- -incognito --user-data-dir --no-sandbox --window-position=0,0 --force-device-scale-factor=1 --disable-dev-shm-usage}"
 
 exec "$@"
